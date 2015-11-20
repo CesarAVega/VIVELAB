e78152c2d1e79f5cc89916c16ea5caa6f620e751
CesarAVega <zzaleman@yahoo.com>
2015-11-20 12:04:02 -0500

    SuperCookie

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject superCookie;
+
+    private GameObject activeSuperCookie;
+
+	// Use this for initialization
+	void Start () {
+        activeSuperCookie = Instantiate(superCookie);
+        StartCoroutine(ScheduleCookieDestroy());
+  	}
+
+    IEnumerator ScheduleCookieDestroy() {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(activeSuperCookie);
+    }
+}
